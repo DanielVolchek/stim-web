d2a965e022dfa34f2b02bafd58fc135561baef60
Daniel Volchek <danielvolchek@Daniels-MacBook-Pro.local>
2023-05-28 11:05:16 -0400

    new migration
    new

---
+++ prisma/migrations/20230528150501_init/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `sessionToken` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "User_sessionToken_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "sessionToken",
+ADD COLUMN     "sessions" TEXT[];
