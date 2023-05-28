a77e89659a6234498eaf99df619058a678f7aef2
Daniel Volchek <danielvolchek@Daniels-MacBook-Pro.local>
2023-05-28 13:54:23 -0400

    added utils file for auth methods

---
+++ src/utils/auth.ts
@@ -0,0 +1,65 @@
+import hash from "crypto-js/md5";
+import { v4 as uuid } from "uuid";
+import prisma from "./prisma";
+import { User, Session } from "@prisma/client";
+
+const generateSession = () => {
+  return uuid();
+};
+
+const hashPassword = (pass: string) => {
+  return hash(pass).toString();
+};
+
+const getSessionByToken = async (token: string) => {
+  const session = await prisma.session.findFirst({ where: { token } });
+  return session;
+};
+
+const getUserBySession = async (session: Session) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      id: {
+        equals: session.userID,
+      },
+    },
+  });
+
+  return validateSessionTime(session) && user;
+};
+
+const createSessionOnUser = async (user: User, sessionToken: string) => {
+  await prisma.session.create({
+    data: {
+      token: sessionToken,
+      userID: user.id,
+      /* 1 month from now */
+      expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+    },
+  });
+
+  await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      sessions: {
+        connect: {
+          token: sessionToken,
+        },
+      },
+    },
+  });
+};
+
+const validateSessionTime = (session: Session) => {
+  if (session.expiresAt.getTime() >= Date.now()) return false;
+};
+
+export {
+  generateSession,
+  hashPassword,
+  getUserBySession,
+  createSessionOnUser,
+  getSessionByToken,
+};
