1e51f1efc54545fcb16a5625b5888f1b93bda5bf
Daniel Volchek <danielvolchek@Daniels-MacBook-Pro.local>
2023-06-01 12:08:18 -0400

    added url code
    updated url code to include /
    
    updated

---
+++ src/utils/url.ts
@@ -0,0 +1,11 @@
+let URL;
+
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") {
+  URL = "https://www.example.com";
+} else {
+  URL = `http://localhost:${PORT}`;
+}
+
+export default URL;
