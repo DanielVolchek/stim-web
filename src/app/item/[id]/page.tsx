d5c8054f4b1cadeafeca7e89b0b23c9a61eb06d5
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-03 11:44:44 -0400

    added dynamic route for items

---
+++ src/app/item/[id]/page.tsx
@@ -0,0 +1,10 @@
+import prisma from "@/utils/prisma";
+type Props = { params: { id: string } };
+
+export default async function Item({ params: { id } }: Props) {
+  const item = await prisma.item.findUnique({ where: { id: parseInt(id) } });
+
+  // on item page check if an item has been rented by the current user
+  // if that is the case render a return item button
+  return <p>{item?.name}</p>;
+}
