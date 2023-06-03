5ac4fe298cf615a950b57565539b4c754854d931
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-03 11:45:00 -0400

    modified strucutre

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
 import { cookies } from "next/dist/client/components/headers";
 import { redirect } from "next/navigation";
 
-import { getURL } from "@/utils/url";
 import { getSession } from "@/utils/auth";
 
 export default async function Home() {
   const cookieStore = cookies();
 
   const session = await getSession(cookieStore);
+  if (!session) return redirect("/login");
+
+  // if admin
+  // redirect to /admin
+  // or render page differently as if you're an admin
+  // if rendering differently than create a zustand store for admin
+
+  // fetch and render items down here
+  // render items down here
+
+  // if rented item render that on top of the page
+
+  // render the rest of the items down here
+  // item should be in a clickable card
+
+  // after clicking redirect to item page
+
+  // on item page
 }
