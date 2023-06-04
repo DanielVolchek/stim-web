f5714e7ca13fdb22fb06983627139bdd01a312a6
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-04 09:35:34 -0400

    modified name and switched to default export

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,6 @@
-export const getURL = () => {
+export default function baseURL() {
   const PORT = process.env.PORT || 3000;
   if (typeof window !== "undefined") return "";
   if (process.env.NODE_ENV === "production") return "https://www.example.com";
   return `http://localhost:${PORT}`;
-};
+}
