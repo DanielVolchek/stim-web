a965b143ec0c8f030b78e1f17ddc34f633d1bbbf
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-04 14:49:32 -0400

    changed role to lowercase

--- src/utils/zod/index.ts
+++ src/utils/zod/index.ts
@@ -3,6 +3,5 @@ import { z } from "zod";
 export const userSchema = z.object({
   id: z.string(),
   username: z.string(),
-  passwordHash: z.string(),
-  Role: z.enum(["ADMIN", "USER"]),
+  role: z.enum(["ADMIN", "USER"]),
 });
