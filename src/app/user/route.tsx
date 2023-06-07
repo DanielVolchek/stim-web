53086592a80cfb1520e07f9233acd232a7925805
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-07 11:24:21 -0400

    modified get session route

--- src/app/user/route.tsx
+++ src/app/user/route.tsx
@@ -1,29 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { authenticationFlow } from "@/utils/auth";
 
-let COUNT = 0;
-
-export async function POST(req: Request) {
-  const body = await req.json();
-
-  let user;
-  try {
-    user = await authenticationFlow(body.session);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Unauthorized: " + error },
-      { status: 401 }
-    );
-  }
-
-  let { passwordHash, ...safeUser } = user;
-  return NextResponse.json({ user: safeUser }, { status: 200 });
-}
-
 export async function GET(req: NextRequest) {
-  // const session = req.cookies.get("session")?.value;
-  console.log("COUNT=", ++COUNT);
-
   const sessionToken = req.headers.get("set-cookie");
 
   const session = parseSessionCookie(sessionToken) as string;
