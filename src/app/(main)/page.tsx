4f2db2e6bca3cf78cc7fcbf7dcbe7042075706e7
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-08 13:38:22 -0400

    started working on home pagte

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,17 @@
 import { redirect } from "next/navigation";
 import { SafeUser, getUserSession } from "@/utils/auth";
-import ItemList from "@/components/ItemList";
+import ItemList from "@/components/Item/ItemList";
 
 export default async function Home() {
-  return <div></div>;
+  return (
+    <main>
+      <h1 className="mt-2 text-center text-6xl">StimStore</h1>
+      <ItemList />
+    </main>
+  );
+
   // return <ItemList />;
   // redirect to /admin
-  // or render page differently as if you're an admin
-  // if rendering differently than create a zustand store for admin
   // fetch and render items down here
   // render items down here
   // if rented item render that on top of the page
