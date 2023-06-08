fb1bf6b912f532427b458dccd2159b41584decdf
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-08 13:35:58 -0400

    work on item card

--- src/components/Item/ItemCard.tsx
+++ src/components/Item/ItemCard.tsx
@@ -1,11 +1,15 @@
 import { Item, RentEvent, Image as ImageType } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
+import RentButton from "./RentButton";
+import { getUserSession } from "@/utils/auth";
 
 type Props = {
   item: Item & { image: ImageType | null; currentRentEvent: RentEvent | null };
 };
-export default function ItemCard({ item }: Props) {
+export default async function ItemCard({ item }: Props) {
+  const user = await getUserSession();
+
   return (
     <Link href={`/items/${item.id}`}>
       <div className="flex basis-1/3 border-2 border-gray-300 p-4">
@@ -15,7 +19,7 @@ export default function ItemCard({ item }: Props) {
           <p>{item.desc}</p>
         </div>
 
-        <RentButton />
+        {/* <RentButton item={item} user={user} /> */}
       </div>
     </Link>
   );
