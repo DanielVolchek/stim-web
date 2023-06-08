e5727936887ec98764717d719361a1ee82c20396
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-08 13:36:40 -0400

    redirect from login if already logged in
    redirect

--- src/app/(authpages)/login/page.tsx
+++ src/app/(authpages)/login/page.tsx
@@ -1,5 +1,11 @@
 import AuthenticationForm from "@/components/Form/AuthenticationForm";
+import { getUserSession } from "@/utils/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const user = await getUserSession();
+
+  if (user) redirect("/");
+
   return <AuthenticationForm type={"login"} />;
 }
--- src/app/(authpages)/register/page.tsx
+++ src/app/(authpages)/register/page.tsx
@@ -1,5 +1,11 @@
 import AuthenticationForm from "@/components/Form/AuthenticationForm";
+import { getUserSession } from "@/utils/auth";
+import { redirect } from "next/navigation";
 
-export default function Register() {
+export default async function Register() {
+  const user = await getUserSession();
+
+  if (user) redirect("/");
+
   return <AuthenticationForm type={"register"} />;
 }
