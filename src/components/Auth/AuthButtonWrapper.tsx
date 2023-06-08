586230a59c6cc08208e98b3aee4db586e57f33c6
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-08 13:38:08 -0400

    added styles

--- src/components/Auth/AuthButtonWrapper.tsx
+++ src/components/Auth/AuthButtonWrapper.tsx
@@ -1,11 +1,19 @@
 type Props = {
   onClick: () => void;
   innerText: string;
+  className?: string;
 };
 
-export default function AuthButtonWrapper({ onClick, innerText }: Props) {
+export default function AuthButtonWrapper({
+  onClick,
+  innerText,
+  className,
+}: Props) {
   return (
-    <button onClick={onClick} className="">
+    <button
+      onClick={onClick}
+      className={`rounded-3xl border-ebony bg-alabaster px-12 py-2 text-copper transition-all hover:-translate-y-[6px] hover:shadow-md hover:shadow-slate-600 ${className}`}
+    >
       {innerText}
     </button>
   );
