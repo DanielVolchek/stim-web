35f90089ae6a7c7cc8f66a4d2b7a121227f1dd99
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-08 13:36:06 -0400

    modified nav style

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import { getUserSession } from "@/utils/auth";
 import LoginActionsContainer from "./Auth/LoginActionsContainer";
+import HomeIcon from "./Icons/Home";
+import Slug from "./Icons/Slug";
 
 export default async function Nav() {
   const user = await getUserSession();
   const isAdmin = user?.role === "ADMIN";
 
   return (
-    <header>
-      <nav className="flex justify-between border-b-4 border-black p-4">
-        <Link href="/">Home</Link>
+    <header className="bg-celadon text-alabaster">
+      <nav className="flex items-center justify-between px-8 py-2">
+        <Link href="/">
+          <Slug className="w-12 -scale-x-100 fill-copper transition-colors duration-[25ms] hover:fill-alabaster" />
+        </Link>
         {isAdmin && <a href="/admin">Admin</a>}
         <div className="flex">
           <LoginActionsContainer />
