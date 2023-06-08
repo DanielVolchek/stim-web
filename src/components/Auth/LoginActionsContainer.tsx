a3ceaf2392bbd66618c6ca26c1ffd0236d7f7f7e
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-08 18:39:53 -0400

    modified LoginButton to get cookie directly

--- src/components/Auth/LoginActionsContainer.tsx
+++ src/components/Auth/LoginActionsContainer.tsx
@@ -7,8 +7,7 @@ export default async function LoginActionsContainer() {
   const user = await getUserSession();
 
   if (user) {
-    const session = cookies().get("session")?.value;
-    return <LogoutButton session={session as string} />;
+    return <LogoutButton />;
   }
 
   return <LoginOrRegisterButtons />;
--- src/components/Auth/LogoutButton.tsx
+++ src/components/Auth/LogoutButton.tsx
@@ -4,7 +4,7 @@ import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import AuthButtonWrapper from "./AuthButtonWrapper";
 
-export default function LogoutButton({ session }: { session: string }) {
+export default function LogoutButton() {
   const router = useRouter();
 
   // const session = Cookies.get("session");
@@ -12,6 +12,7 @@ export default function LogoutButton({ session }: { session: string }) {
   // if (!session) router.push("/");
 
   const onClick = () => {
+    const session = Cookies.get("session");
     Cookies.remove("session");
     router.push(`/logout?session=${session}`);
   };
