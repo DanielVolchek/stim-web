ab77619182282d0d1b1ef76bbf64aff1c5a21dc5
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-19 13:00:25 -0400

    renamed clienterrordisplay to clientmessagedisplay

---
+++ src/app/ClientMessageDisplay.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Popup from "@/components/Popup";
+import useClientMessageStore from "@/utils/useClientMessageStore";
+
+export default function ClientMessageDisplay() {
+  const { clientMessage, updateClientMessage } = useClientMessageStore();
+
+  return clientMessage ? (
+    <Popup onClose={() => updateClientMessage(undefined)}>
+      <h1 className="text-4xl">{clientMessage.type}: </h1>
+      <span className="text-2xl">{clientMessage.message}</span>
+    </Popup>
+  ) : null;
+}
