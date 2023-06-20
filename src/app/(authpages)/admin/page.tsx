9b4e343e44188987b4cbac3c8a08b3e7f5027fa7
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-20 06:30:17 -0400

    add button to go to form

--- src/app/(authpages)/admin/page.tsx
+++ src/app/(authpages)/admin/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Admin() {
-  return <h1>You are an admin</h1>;
+  return (
+    <main>
+      <h1 className="mb-5">ADMIN</h1>
+      <Link
+        href="/items/add"
+        className="hoverEffect ${className} rounded-3xl border border-copper bg-alabaster px-12 py-2  text-copper hover:shadow-copper"
+      >
+        Add item
+      </Link>
+    </main>
+  );
 }
