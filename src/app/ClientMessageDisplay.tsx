6f6a9869bc6b7a70293188ab136cb2255fa65a23
Daniel Volchek <danielvolchek123@gmail.com>
2023-06-20 06:33:26 -0400

    changed to a higher z index

--- src/app/ClientMessageDisplay.tsx
+++ src/app/ClientMessageDisplay.tsx
@@ -7,7 +7,7 @@ export default function ClientMessageDisplay() {
   const { clientMessage, updateClientMessage } = useClientMessageStore();
 
   return clientMessage ? (
-    <Popup onClose={() => updateClientMessage(undefined)}>
+    <Popup onClose={() => updateClientMessage(undefined)} className="!z-20">
       <h1 className="text-4xl">{clientMessage.type}: </h1>
       <span className="text-2xl">{clientMessage.message}</span>
     </Popup>
